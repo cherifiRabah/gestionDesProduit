8e58291da309bf0822542243ba9d7938229eeceb
cherifi rabah <cherifi.rabah@yahoo.com>
2018-02-10 15:56:47 +0100

    debut du projet

---
+++ produit.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  sam. 10 fév. 2018 à 15:35
+-- Version du serveur :  10.1.26-MariaDB
+-- Version de PHP :  7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `gestiondesproduits`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `id` bigint(20) NOT NULL,
+  `designation` varchar(255) DEFAULT NULL,
+  `prix` double NOT NULL,
+  `quantite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/java/org/sid/DemoApplication.java
@@ -0,0 +1,23 @@
+package org.sid;
+
+import org.sid.dao.ProduitRepository;
+import org.sid.enteties.Produit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx= SpringApplication.run(DemoApplication.class, args);
+		ProduitRepository produitRepository=ctx.getBean(ProduitRepository.class);
+		produitRepository.save(new Produit("HP", 3000, 50));
+		produitRepository.save(new Produit("dell", 2000, 20));
+		produitRepository.save(new Produit("acer", 1000, 10));
+		produitRepository.save(new Produit("mac", 7000, 90));
+		
+		produitRepository.findAll().forEach(p->System.out.println(p.getDesignation()));
+	}
+}
